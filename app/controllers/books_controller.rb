92660b973f884ef6d3a457f12edaf9cd6ad4466c
niahikawakairi <kairi.0109@au.com>
2022-04-06 14:38:39 +0000

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,9 +8,12 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id)
+    else
+    render :index
+    end
   end
 
   def show
@@ -25,6 +28,12 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
